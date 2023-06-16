7f9d7eeec36c19e3d6a462b781851d142b6446a0
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2023-06-16 16:06:01 -0300

    ajustes

--- wp-content/themes/desconectarviajes/modules/blocks/items/banner_item.php
+++ wp-content/themes/desconectarviajes/modules/blocks/items/banner_item.php
@@ -2,5 +2,5 @@
     <div class="image-background">
         <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
     </div>
-    <?php echo ($title = get_sub_field('title'))? '<h2 class="grid__title h5">'.$title.'</h2>':'';?>
+    <?php echo ($title = get_sub_field('title'))? '<h2 class="grid__title h4">'.$title.'</h2>':'';?>
 </div>
