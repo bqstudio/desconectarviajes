01073e8108692dab0d4f2d0c3cd3296c9ae52d4a
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2023-06-20 18:32:51 -0300

    block-banner-image

--- wp-content/themes/desconectarviajes/modules/blocks/banner-image/block.php
+++ wp-content/themes/desconectarviajes/modules/blocks/banner-image/block.php
@@ -21,20 +21,21 @@
 do_action( 'palermo_pre_render_block', $block );
 ?>
 <section class="wrapper padding-b bg-color">
-    <section class="module banner banner--image" id="ofertas">
-        <div class="image-background">
-            <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
-        </div>
-        <div class="banner--image__content">
-            <div class="banner--image__text">
-                <?php echo ($title = get_field('title'))? '<h2 class="banner__title h3 ">'.$title.'</h2>':'';
-                echo ($text = get_field('text'))? '<div class="banner__text">'.$text.'</div>':''; ?>
-            </div>
-            <div class="banner--image__flyer">
-                <div class="image-background">
-                    <?php get_template_part('modules/components/image',NULL,array('image_flyer' => get_field('flyer')) ); ?>
+    <div class="module banner banner--image" id="ofertas">
+        <div class="container">
+            <div class="banner__content">
+                <div class="banner__flyer">
+                    <div class="image-background">
+                        <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
+                    </div>
+                </div>
+                <div class="banner__description">
+                    <div class="banner__text">
+                        <?php echo ($title = get_field('title'))? '<h2 class="bannerimage__title h3 ">'.$title.'</h2>':'';
+                        echo ($text = get_field('text'))? '<div class="banner__text">'.$text.'</div>':''; ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 </section>
